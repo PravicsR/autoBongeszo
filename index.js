ff6103995415e001359df29c31ca33474a7db069
PravicsR <pravcsroland@gmail.com>
2023-12-02 16:48:48 +0100

    modified:   index.js 	modified:   package.json

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
+//futtatas: node .\index.js
+
 import puppeteer from 'puppeteer';
 
 (async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    headless: false,
+    defaultViewport: false,
+    userDataDir: "./temp"
+    });
   const page = await browser.newPage();
 
-  // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
+  await page.goto('https://npmjs.com/');
 
-  // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
+  await page.type('._390acbc5', 'express');
 
-  // Type into search box
-  await page.type('.search-box__input', 'automate beyond recorder');
-
-  // Wait and click on first result
-  const searchResultSelector = '.search-box__link';
+  const searchResultSelector = '._5b436c1a';
   await page.waitForSelector(searchResultSelector);
   await page.click(searchResultSelector);
 
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize and automate'
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
+  var selector = '#top > div.fdbf4038.w-third-l.mt3.w-100.ph3.ph4-m.pv3.pv0-l > div:nth-child(7) > div > p';
+  await page.waitForSelector(selector);
+  let element = await page.$(selector);
+  let value = await page.evaluate(el => el.textContent, element);
+  console.log("Current version is: " + value);
 
   await browser.close();
-})();
+})(); 
